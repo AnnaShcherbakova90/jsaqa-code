8120bc579aecc87d28e2ca216c595d03c96fd637
Anna Shcherbakova <ShcherbakovaAnna90@yandex.ru>
2024-07-09 00:11:24 +0500

    second part of home task

--- 7.5/puppeteer-2/net.test.js
+++ 7.5/puppeteer-2/net.test.js
@@ -1,4 +1,9 @@
-// const { clickElement, putText, getText } = require("./lib/commands.js");
+const {
+  clickElement,
+  putText,
+  getText,
+  waitForPageLoaded,
+} = require("./lib/commands.js");
 // const { generateName } = require("./lib/util.js");
 
 let page;
@@ -14,97 +19,45 @@ afterEach(async () => {
 });
 
 describe("Netology.ru tests", () => {
-  test("The first test", async () => {
-    const dayLink = await page.$("a[data-time-stamp='1720386000']");
-    await dayLink.click();
-    const seanceLink = await page.$("a[data-seance-id='218']");
-    await seanceLink.click();
+  test("First test: Booking a movie ticket", async () => {
+    await clickElement(page, "a[data-time-stamp='1720731600']");
+    await clickElement(page, "a[data-seance-id='218']");
+    waitForPageLoaded;
 
-    await page.waitForNavigation();
-
-    const chosenMovieNameElement = await page.$("h2.buying__info-title");
-    const chosenMovieName = await page.evaluate(
-      (chosenMovieNameElement) => chosenMovieNameElement.textContent,
-      chosenMovieNameElement
-    );
+    const chosenMovieName = await getText(page, "h2.buying__info-title");
     expect(chosenMovieName).toContain("Микки маус");
-  }, 50000);
+  }, 500000);
 
-  test("The second test", async () => {
-    const dayLink = await page.$("a[data-time-stamp='1720386000']");
-    await dayLink.click();
-    const seanceLink = await page.$("a[data-seance-id='218']");
-    await seanceLink.click();
-
-    await page.waitForNavigation();
-
-    console.log("page " + page.url());
-    const seatElement = await page.$(
+  test("Second test: Selecting a seat and booking", async () => {
+    await clickElement(page, "a[data-time-stamp='1720731600']");
+    await clickElement(page, "a[data-seance-id='218']");
+    waitForPageLoaded;
+    await clickElement(
+      page,
       "span[class='buying-scheme__chair buying-scheme__chair_standart']"
     );
-
-    await seatElement.evaluate((seatElement) => seatElement.click());
-    const bookButton = await page.waitForSelector("button.acceptin-button");
-    await bookButton.click();
-
-    await page.waitForNavigation();
-
-    const bookedMovieNameElement = await page.$(
-      "span[class='ticket__details ticket__title']"
-    );
-    const bookedMovieName = await page.evaluate(
-      (bookedMovieNameElement) => bookedMovieNameElement.textContent,
-      bookedMovieNameElement
+    await clickElement(page, "button.acceptin-button");
+    waitForPageLoaded;
+    const bookedMovieName = await getText(
+      page,
+      "span.ticket__details.ticket__title"
     );
     expect(bookedMovieName).toContain("Микки маус");
   }, 70000);
 
-  test("The third test", async () => {
-    const dayLink = await page.$("a[data-time-stamp='1720386000']");
-    await dayLink.click();
-    const seanceLink = await page.$("a[data-seance-id='218']");
-    await seanceLink.click();
+  test("Third test: Checking seat availability", async () => {
+    await clickElement(page, "a[data-time-stamp='1720731600']");
+    await clickElement(page, "a[data-seance-id='218']");
+    waitForPageLoaded;
 
-    await page.waitForNavigation();
-
-    console.log("page " + page.url());
-    const seatElement = await page.$(
+    await clickElement(
+      page,
       "span[class='buying-scheme__chair buying-scheme__chair_taken']"
     );
 
-    await seatElement.evaluate((seatElement) => seatElement.click());
-
     const bookButton = await page.$("button[class='acceptin-button']");
-
-    const isDisabled = await bookButton.evaluate(
-      (bookButton) => bookButton.disabled
-    );
+    const isDisabled = await bookButton.evaluate((btn) => btn.disabled);
 
     expect(isDisabled).toEqual(true);
   }, 50000);
 });
-
-//   test("The first link text 'Медиа Нетологии'", async () => {
-//     const actual = await getText(page, "header a + a");
-//     expect(actual).toContain("Медиа Нетологии");
-//   });
-
-//   test("The first link leads on 'Медиа' page", async () => {
-//     await clickElement(page, "header a + a");
-//     const actual = await getText(page, ".logo__media");
-//     await expect(actual).toContain("Медиа");
-//   });
-// });
-
-// test("Should look for a course", async () => {
-//   await page.goto("https://netology.ru/navigation");
-//   await putText(page, "input", "тестировщик");
-//   const actual = await page.$eval("a[data-name]", (link) => link.textContent);
-//   const expected = "Тестировщик ПО";
-//   expect(actual).toContain(expected);
-// });
-
-// test("Should show warning if login is not email", async () => {
-//   await page.goto("https://netology.ru/?modal=sign_in");
-//   await putText(page, 'input[type="email"]', generateName(5));
-// });
